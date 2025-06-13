06b5c6def5976b1a6c2b084289424568b98e6e15
Boris Pardo <boris.pardo8021@utc.edu.ec>
2025-06-12 22:42:25 -0500

    Configuracion del modelo puntointeres

--- app/Models/puntointeres.php
+++ app/Models/puntointeres.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class puntointeres extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'categoria',
+        'imagen',
+        'latitud',
+        'longitud',
+    ];
 }
